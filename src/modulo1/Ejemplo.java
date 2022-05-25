47c36dc81f3d4e8d7594012f5b7108391c21711d
Jose Adria Maijo <josemaria.adria@allianz.es>
2022-05-25 15:46:12 +0200

    primer ejercicio

---
+++ src/modulo1/Ejemplo.java
@@ -0,0 +1,13 @@
+package modulo1;
+
+public class Ejemplo {
+	public static void main(String[] args) {
+ 
+		System.out.println("hola josep");
+		System.out.println("\n\n\nnombre\t\t\tapellido\t\t\temail\t\t\ttelefono");
+        System.out.println("\njosep\t\t\tadria\t\t\t\tjosepadri@gmail.com\t678453214");
+        System.out.println("\nsergi\t\t\tadria\t\t\t\tsergipadrig@gmail.com\t564543432");
+        System.out.println("\nroser\t\t\tguerra\t\t\t\troserguerrag@gmail.com\t654253574");
+	}
+
+}
